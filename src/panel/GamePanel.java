65d3f3a631ad53b2c24c51635ebbeea76b814d02
Cristian <cristiankhan23@gmail.com>
2024-07-29 11:06:21 -0300

    Add class constants

--- src/panel/GamePanel.java
+++ src/panel/GamePanel.java
@@ -5,9 +5,21 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class GamePanel {
+public class GamePanel extends JPanel implements Runnable {
+  
+  static final int GAME_WIDTH = 1000;
+  
+  static final int GAME_HEIGHT = (int)(GAME_WIDTH * (5/9));
 
-  GamePanel() {
+  static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+  
+  static final int BALL_DIAMETER = 20;
+  
+  static final int PANEL_WIDTH = 25;
+  
+  static final int PANEL_HEIGHT = 1000;
+  
+  public GamePanel() {
     
   }
   
