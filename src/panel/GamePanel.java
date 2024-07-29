1d8f43266d616d8419f952ee522d923385963875
Cristian <cristiankhan23@gmail.com>
2024-07-29 06:47:46 -0300

    Imports and create actions methods

--- src/panel/GamePanel.java
+++ src/panel/GamePanel.java
@@ -1,5 +1,52 @@
 package panel;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
 public class GamePanel {
 
+  GamePanel() {
+    
+  }
+  
+  public void newBall() {
+    
+  }
+  
+  public void newPaddles() {
+    
+  }
+  
+  public void paint(Graphics graphic) {
+    
+  }
+  
+  public void draw(Graphics graphic) {
+    
+  }
+  
+  public void move() {
+    
+  }
+  
+  public void checkCollision() {
+    
+  }
+  
+  public void run() {
+    
+  }
+  
+  public class AL extends KeyAdapter {
+    
+    public void keyPressed(KeyEvent e) {
+      
+    }
+    
+    public void keyReleased(KeyEvent e) {
+      
+    }
+  }
 }
