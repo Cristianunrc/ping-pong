112b815751f83acaebb187d70255533f43042327
Cristian <cristiankhan23@gmail.com>
2024-08-05 21:58:19 -0300

    Implement methods

--- src/panel/Ball.java
+++ src/panel/Ball.java
@@ -7,30 +7,46 @@ import javax.swing.*;
 
 public class Ball extends Rectangle {
   
-  Random random;
+  private Random random;
   
   int xVelocity;
   
   int yVelocity;
   
-  public Ball() {
+  private int initialSpeed = 3;
+  
+  public Ball(int x, int y, int width, int height) {
+    super(x, y, width, height);
+    random = new Random();
+    int randomXDirection = random.nextInt(2);
+    if (randomXDirection == 0) {
+      randomXDirection--;
+    }
+    setXDirection(randomXDirection * this.initialSpeed);
     
+    int randomYDirection = random.nextInt(2);
+    if (randomYDirection == 0) {
+      randomYDirection--;
+    }
+    setYDirection(randomYDirection * this.initialSpeed);
   }
   
   public void setXDirection(int randomXDirection) {
-    
+    this.xVelocity = randomXDirection;
   }
   
   public void setYDirection(int randomYDirection) {
-    
+    this.yVelocity = randomYDirection;
   }
   
   public void move() {
-    
+    x += this.xVelocity;
+    y += this.yVelocity;
   }
   
-  public void draw(Graphics graphic) {
-    
+  public void draw(Graphics g) {
+    g.setColor(Color.white);
+    g.fillOval(x, y, height, width);
   }
 
 }
